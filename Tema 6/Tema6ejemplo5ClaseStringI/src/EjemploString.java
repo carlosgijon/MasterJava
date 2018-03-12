765d6bf7c0dda7a9a632d4beca9b382e3feac9b6
Carlos Gijon <carlosgijonfernandez@gmail.com>
2018-03-12 11:10:21 +0100

    Subida nuevo ejemplo 5 CLASE STRING I

---
+++ Tema 6/Tema6ejemplo5ClaseStringI/src/EjemploString.java
@@ -0,0 +1,10 @@
+public class EjemploString {
+    public static void main(String[] args) {
+        String texto = "Clase String";
+        System.out.println(texto);
+
+        // Vamos a añadir "Java" al final de la cadena texto
+        texto += " Java";
+        System.out.println(texto);
+    }
+}
