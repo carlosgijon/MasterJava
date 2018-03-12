14e473858807d9f46a56ec2a19cef230f77b0674
Carlos Gijon <carlosgijonfernandez@gmail.com>
2018-03-12 12:00:42 +0100

    Nuevo ejemplo subido. FLUJOS

---
+++ Tema 6/Tema6ejemplo6Flujos/src/LecturaTeclado.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+public class LecturaTeclado {
+    public static void main(String[] args) throws IOException {  // Siempre que se trabaja con I/O se pone esta excepcion
+        String nombreUsuario;
+
+        System.out.println("Introduzca un nombre:");
+
+        // Se crea un objeto de la clase BufferedReader
+        BufferedReader lector = new BufferedReader(new InputStreamReader(System.in));
+
+        // Se utliza el metodo readLine() para introducir una linea de caracteress. Con el método read() solo se introduce un caracter
+        nombreUsuario = lector.readLine();
+        System.out.println("Bienvenido a nuestra aplicacion " +nombreUsuario+ "!!!");
+    }
+}
