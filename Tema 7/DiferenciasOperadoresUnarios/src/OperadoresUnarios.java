8ff0b11e44d03547fee37292bf9a654539489e51
Carlos Gijon <carlosgijonfernandez@gmail.com>
2018-03-15 09:37:28 +0100

    Nueva subida: diferencia entre operador unario delante y detras

---
+++ Tema 7/DiferenciasOperadoresUnarios/src/OperadoresUnarios.java
@@ -0,0 +1,9 @@
+public class OperadoresUnarios {
+    public static void main(String[] args) {
+        int contador = 0;
+        System.out.println("++contador da " + ++contador);
+
+        contador = 0;
+        System.out.println("contador++ da "+ contador++);
+    }
+}
