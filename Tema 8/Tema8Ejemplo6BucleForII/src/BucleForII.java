8c6bbc411da64bd80b40414e91b17635005d4314
Carlos Gijon <carlosgijonfernandez@gmail.com>
2018-03-21 11:24:08 +0100

    nueva subida: Bucle For Version 2

---
+++ Tema 8/Tema8Ejemplo6BucleForII/src/BucleForII.java
@@ -0,0 +1,10 @@
+public class BucleForII {
+    public static void main(String[] args) {
+        System.out.println("Voy a entrar al bucle");
+        System.out.println("Numeros pares");
+        for(int i = 0; i < 20; i += 2) {
+            System.out.println("Valor de i: " + i);
+        }
+        System.out.println("He salido del bucle. Fin del programa");
+    }
+}
