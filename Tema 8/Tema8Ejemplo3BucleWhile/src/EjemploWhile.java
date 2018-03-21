4e68df94f6003a36d005eca4531d6010ce686875
Carlos Gijon <carlosgijonfernandez@gmail.com>
2018-03-21 10:56:14 +0100

    Nuevas subidas

---
+++ Tema 8/Tema8Ejemplo3BucleWhile/src/EjemploWhile.java
@@ -0,0 +1,12 @@
+public class EjemploWhile {
+    public static void main(String[] args) {
+        String mensaje = "";
+        int i = 0;
+
+        while(i <= 10) {
+            i++;
+            System.out.println("Valor de i: " + i);
+        }
+        System.out.println("Se ha salido del bucle");
+    }
+}
---
+++ Tema 8/Tema8Ejemplo4BucleDoWhile/src/BucleDoWhile.java
@@ -0,0 +1,14 @@
+public class BucleDoWhile {
+    public static void main(String[] args) {
+        int i = 20;
+        System.out.println("Voy a entrar al bucle");
+
+        do {
+            i++;
+            System.out.println("Valor de i = " + i);
+
+        } while(i <= 10);
+
+        System.out.println("He salido del bucle");
+    }
+}
