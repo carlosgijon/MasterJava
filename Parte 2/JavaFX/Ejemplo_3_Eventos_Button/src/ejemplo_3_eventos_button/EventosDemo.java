27e5a6539325f3c104707f7e9b182e35eebd3ede
carlosgijon <carlosgijonfernandez@gmail.com>
2018-05-29 10:08:41 +0200

    Nueva subida: Ejemplo de Button y eventos en JavaFX

---
+++ Parte 2/JavaFX/Ejemplo_3_Eventos_Button/src/ejemplo_3_eventos_button/EventosDemo.java
@@ -0,0 +1,62 @@
+package ejemplo_3_eventos_button;
+
+import javafx.application.*;
+import javafx.scene.*;
+import javafx.stage.*;
+import javafx.scene.layout.*;
+import javafx.scene.control.*;
+import javafx.event.*;
+import javafx.geometry.*;
+
+public class EventosDemo extends Application {
+    
+    Label lbl_etiqueta;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+    // Sobreescribimos el metodo start()
+    @Override
+    public void start(Stage escenario) {
+        escenario.setTitle("Usando Botones en JavaFx");
+        FlowPane nodo_raiz = new FlowPane(10,10);
+        nodo_raiz.setAlignment(Pos.CENTER);
+        
+        // Creamos la escena
+        Scene escena = new Scene(nodo_raiz, 300, 100);
+        
+        // asignamos la escena al escenario
+        escenario.setScene(escena);
+        
+        // creamos la etiqueta
+        lbl_etiqueta = new Label("Presiona un botón");
+        
+        // Creamos dos botones
+        Button btn_arriba = new Button("arriba");
+        Button btn_abajo = new Button("abajo");
+        
+        // Asignamos los eventos a cada botón, primero con clase interna
+        // y despues con expresión lambda
+        
+        // Clase interna
+        btn_arriba.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent ae) {
+                lbl_etiqueta.setText("has presionado botón arriba");
+            }
+        });
+        
+        // Con expresión Lambda
+        btn_abajo.setOnAction((ActionEvent ae) -> {
+            lbl_etiqueta.setText("Has presionado botón abajo");
+        });
+        
+        // Añadir a la escena los elementos gráficos
+        nodo_raiz.getChildren().addAll(btn_arriba, btn_abajo,lbl_etiqueta);
+        
+        
+        // Mostrar escenario
+        escenario.show();
+    }
+}
