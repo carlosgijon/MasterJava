85668cfd756722e7626fa5a31e1574e48950c20d
carlosgijon <carlosgijonfernandez@gmail.com>
2018-06-06 10:17:27 +0200

    Nueva subida: Ejemplo 3 métodos de Socket

---
+++ Parte 2/Tema 7/Ejemplo_3_metodos_Socket/src/ejemplo_3_metodos_socket/MetodosSocket.java
@@ -0,0 +1,42 @@
+package ejemplo_3_metodos_socket;
+
+import java.io.*;
+import java.net.*;
+
+public class MetodosSocket {
+
+    public static void main(String[] args) {
+        try {
+            Socket mi_socket = new Socket("200.200.1.12",8810);
+            BufferedReader entrada = new BufferedReader(new InputStreamReader(mi_socket.getInputStream()));
+            
+            // Leer los datos
+            String texto = entrada.readLine();
+            System.out.println(texto);
+            
+            PrintWriter out = new PrintWriter(mi_socket.getOutputStream(), true);
+            
+            out.println("Hola");
+            
+            InetAddress a = mi_socket.getInetAddress();
+            System.out.println("mi_socket.getInetAddress(): " +a);
+            System.out.println("Puerto al que estamos conectados: " +mi_socket.getPort());
+            
+            a = mi_socket.getLocalAddress();
+            System.out.println("mi_socket.getLocalAddress: " +a);
+            System.out.println("Puerto desde el que estamos conectados: " +mi_socket.getLocalPort());
+            
+            // Cerramos el socket
+            mi_socket.close();
+        }
+        catch(UnknownHostException exc_unknow_host) {
+            System.out.println("No se encuentra el servidor");
+        }
+        
+        catch(IOException exc_io) {
+            System.out.println("Error E/S");
+        }
+        
+    }
+
+}
