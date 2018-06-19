33ca54247517b7182b858e5d6712f8bcc2a6da71
carlosgijon <carlosgijonfernandez@gmail.com>
2018-06-19 10:30:18 +0200

    Nueva subida: Ejemplo 4 ejemplo de Servlet (no funciona)

---
+++ Parte 3/Tema 4 Desarrollando Servlets/Tema_4_Ejemplo_1_estructura_servlet/src/java/EjemploServlet.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class EjemploServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse respuesta) throws ServletException, IOException {
+        ProcessRequest(request, respuesta);
+    } 
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse respuesta) throws ServletException, IOException {
+        ProcessRequest(request, respuesta);
+    }
+}
